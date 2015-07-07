cd6f26f5b3f19b04ba423bf59c7072748f1d360d
Juampy <juampy72@gmail.com>
2015-07-07 12:48:56 +0200

    Document and simplify server.js

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+/**
+ * React form server.The simplest server ever.
+ *
+ * This Node.js application serves the contact form with its
+ * assets and processes the submission. A lot of aspects
+ * of a real life application have been omitted here
+ * for the sake of simplicity such as form validation and
+ * security.
+ */
 var http = require('http');
 var fs = require('fs');
 
-var files = {
-
-}
 http.createServer(function (req, res) {
+  // Form handler.
   if (req.url == '/') {
     fs.readFile('./index.html', function (err, html) {
       if (err) {
@@ -15,21 +22,17 @@ http.createServer(function (req, res) {
       res.end();
     });
   }
+  // Assets handler.
+  else if (['/js/contact-form.js', '/js/JSXTransformer.js', '/js/react.js'].indexOf(req.url) !== -1) {
+    fs.readFile('./' + req.url, function (err, js) {
+      res.writeHeader(200, {'Content-Type': 'application/javascript'});
+      res.write(js);
+      res.end();
+    });
+  }
+  // Form submit handler.
   else if (req.url == '/send') {
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify({status: 'OK' }));
-  }
-  else {
-    fs.readFile('./' + req.url, function (err, js) {
-      if (err) {
-        console.log(req.url);
-        res.end();
-      }
-      else {
-        res.writeHeader(200, {'Content-Type': 'application/javascript'});
-        res.write(js);
-        res.end();
-      }
-    });
   }
 }).listen(1337, '127.0.0.1');
