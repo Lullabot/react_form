63ce97740ef1e749047396e9dc1f8c8566c207cf
Juampy <juampy72@gmail.com>
2015-07-08 11:46:05 +0200

    Extract POST data server side

--- server.js
+++ server.js
@@ -9,9 +9,10 @@
  */
 var http = require('http');
 var fs = require('fs');
+var qs = require('querystring');
 
 http.createServer(function (req, res) {
-  // Form handler.
+  // Returns the form.
   if (req.url == '/') {
     fs.readFile('./index.html', function (err, html) {
       if (err) {
@@ -22,7 +23,7 @@ http.createServer(function (req, res) {
       res.end();
     });
   }
-  // Assets handler.
+  // Returns JavaScript assets.
   else if (['/js/contact-form.js', '/js/JSXTransformer.js', '/js/react.js'].indexOf(req.url) !== -1) {
     fs.readFile('./' + req.url, function (err, js) {
       res.writeHeader(200, {'Content-Type': 'application/javascript'});
@@ -30,9 +31,21 @@ http.createServer(function (req, res) {
       res.end();
     });
   }
-  // Form submit handler.
-  else if (req.url == '/send') {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({status: 'OK' }));
+  // Processes the form submission.
+  else if (req.url == '/send' && req.method == 'POST') {
+    var body = '';
+    req.on('data', function (data) {
+      body += data;
+    });
+    req.on('end', function () {
+      var formData = qs.parse(body);
+      // Here we would send an email using a module such as
+      // https://www.npmjs.com/package/emailjs. For simplicity
+      // we will just log the submitted data and return a
+      // successfull response to the client.
+      console.log(formData);
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify({status: 'OK' }));
+    });
   }
 }).listen(1337, '127.0.0.1');
