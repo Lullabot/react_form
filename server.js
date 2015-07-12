ee43cb16748c4672843f32a9c559b892ff0db4d3
Juampy <juampy72@gmail.com>
2015-07-12 12:00:06 +0200

    Use express as the web application

--- server.js
+++ server.js
@@ -1,38 +1,42 @@
 /**
  * React form server.The simplest server ever.
  *
- * This Node.js application serves the contact form with its
+ * This Express application serves the contact form with its
  * assets and processes the submission. A lot of aspects
  * of a real life application have been omitted here
  * for the sake of simplicity such as form validation and
  * security.
  */
-var http = require('http');
-var fs = require('fs');
 
-http.createServer(function (req, res) {
-  // Form handler.
-  if (req.url == '/') {
-    fs.readFile('./index.html', function (err, html) {
-      if (err) {
-        throw err;
-      }
-      res.writeHeader(200, {'Content-Type': 'text/html'});
-      res.write(html);
-      res.end();
-    });
-  }
-  // Assets handler.
-  else if (['/js/contact-form.js', '/js/JSXTransformer.js', '/js/react.js'].indexOf(req.url) !== -1) {
-    fs.readFile('./' + req.url, function (err, js) {
-      res.writeHeader(200, {'Content-Type': 'application/javascript'});
-      res.write(js);
-      res.end();
-    });
-  }
-  // Form submit handler.
-  else if (req.url == '/send') {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({status: 'OK' }));
-  }
-}).listen(1337, '127.0.0.1');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var express = require('express');
+var app = express();
+
+// Returns the contact form.
+app.get('/', function (req, res) {
+  fs.readFile('./index.html', function (err, html) {
+    if (err) {
+      throw err;
+    }
+    res.writeHeader(200, {'Content-Type': 'text/html'});
+    res.write(html);
+    res.end();
+  });
+});
+
+// Exposes public assets such as JavaScript files.
+app.use(express.static('public'));
+
+// Support for JSON-encoded bodies used in POST requests.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.post('/send', function (req, res) {
+  console.log(req.body);
+  return res.send({status: 'OK'});
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+});
