2feb3591c87358108c324aa272433f28d6433ca9
Juampy <juampy72@gmail.com>
2015-07-23 06:52:36 +0200

    Add an example of how to send the form data by email

--- server.js
+++ server.js
@@ -33,6 +33,26 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // Processes the form submission.
 app.post('/send', function (req, res) {
+  // Here is an example of how we could send the form data by email.
+  // var Email = require('emailjs/email');
+  // var server = Email.server.connect({
+  //   host: 'localhost'
+  // });
+  // server.send({
+  //   'text': body,
+  //   'from': req.body.email,
+  //   'to': someone@example.com,
+  //   'reply-to': req.body.email,
+  //   'subject': 'Tell us about your project: ' + req.body.company
+  // }, function(error) {
+  //   if (error) {
+  //     return res.send({status: 'KO'});
+  //   } else {
+  //     return res.send({status: 'OK'});
+  //   }
+  // });
+
+  // For this example, we just log the form data and return OK.
   console.log(req.body);
   return res.send({status: 'OK'});
 });
